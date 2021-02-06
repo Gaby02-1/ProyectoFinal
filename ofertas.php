2aadb9a34a15d432756514b07e94b55a8dd54e7f
unknown <rossberna7@gmail.com>
2021-02-06 00:16:10 -0500

    Archivos del Proyecto

--- ofertas.php
+++ ofertas.php
@@ -4,17 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.79.0">
-    <title>Pricing example · Bootstrap v5.0</title>
+    <title>Ofertas</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/pricing/">
-
-    
-
-    <!-- Bootstrap core CSS -->
-<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
-
+ 
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    </head>
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -32,10 +27,8 @@
     </style>
 
     
-    <!-- Custom styles for this template -->
     <link href="css/pricing.css" rel="stylesheet">
-  </head>
-  <body>
+     <body>
     
 <header>
 </header>
@@ -53,7 +46,7 @@
         <h4 class="my-0 fw-normal">Llanta Aro 16 205/55R16 PRIMEWELL</h4>
       </div>
       <div class="card-body">
-        <img class="card-title pricing-card-title" src="Imagen/llantass.webp">s/226 <small class="text-muted">/ Precio Antiguo</small></h1>
+        <img class="card-title pricing-card-title" src="Imagen/llantass.webp">s/226 <small class="text-muted">/ Precio Antiguo</small>
         <ul class="list-unstyled mt-3 mb-4">
           <li>Diseño: PS880</li>
           <li>Medidas: 205/55R16</li>
@@ -70,7 +63,7 @@
         <h4 class="my-0 fw-normal">Faros LED para automóviles H4 H1 H7</h4>
       </div>
       <div class="card-body">
-        <img class="card-title pricing-card-title" src="Imagen/foros.webp">s/59.00 <small class="text-muted">/ mo</small></h1>
+        <img class="card-title pricing-card-title" src="Imagen/foros.webp">s/59.00 <small class="text-muted">/ mo</small>
         <ul class="list-unstyled mt-3 mb-4">
           <li>Tipo de chip LED: chips COB.</li>
              <li> Vida útil: 35000 horas.</li>
@@ -83,45 +76,6 @@
         </div>
      </div>
            
-  <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
-    <div class="col">
-      <div class="card mb-4 shadow-sm">
-      <div class="card-header">
-        <h4 class="my-0 fw-normal">BATERIA HY H70600 (70AH/600A)</h4>
-      </div>
-      <div class="card-body">
-        <img class="card-title pricing-card-title" src="Imagen/bateriaa.webp">s/329.90 <small class="text-muted">/ Precio Antiguo</small></h1>
-        <ul class="list-unstyled mt-3 mb-4">
-          <li>Libre mantenimiento. </li>
-          <li>Cap. Nominal 166 Amperios/Hora.</li>
-          <li>Voltaje: 12 Voltios.</li>
-          <li>Capacidad de reserva 115 minutos</li>
-        </ul>
-        <button type="button" class="w-100 btn btn-lg btn-outline-primary">S/260.00</button>
-      </div>
-    </div>
-    </div>
-    <div class="col">
-      <div class="card mb-4 shadow-sm">
-      <div class="card-header">
-        <h4 class="my-0 fw-normal">Pegatinas puerta de coche anticolisión de Gaza </h4>
-      </div>
-      <div class="card-body">
-        <img class="card-title pricing-card-title" src="Imagen/pegatina.webp">s/65.46 <small class="text-muted">/ mo</small></h1>
-        <ul class="list-unstyled mt-3 mb-4">
-          <li>durable, resistente al agua, y no se borran.</li>
-             <li>Es conveniente para el vario tipo de coche.</li>
-             <li> Diseño portátil y simple, pequeño tamaño</li> 
-            
-
-            </ul>
-           <button type="button" class="w-100 btn btn-lg btn-primary">s/48.90</button>
-          </div>
-        </div>
-     </div>
-  <footer>
-  </footer>
-</main>
- 
-  </body>
+  
+    </body>
 </html>
