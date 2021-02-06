882d8ffee23bf5bef87d4f6037c1096b66799bbf
unknown <rossberna7@gmail.com>
2021-02-05 23:56:06 -0500

    Archivos del Proyecto

--- index.php
+++ index.php
@@ -1,16 +1,79 @@
-<!DOCTYPE html>
-<html>
-<heasd>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compa" content="ie=edge">
-<title></title>
-</head>
-
-<body>
-
-<?php 
-include("principal.php");
+<?php
+include "configs/config.php";
+include "configs/funciones.php";
+if(!isset($p)){
+    $p="principal";
+}else{
+    $p=$p;
+}
 ?>
-  </body>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0,maximum-scale=1.0, minimum-scale=1.0" >
+    <title>REPUESTOS Y Autopartes CARS SITES</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="fontawesome/css/all.css">
+
+    <script type="text/javacript" src="bootstrap/js/bootstrao.js"></script>
+    <script type="text/javacript" src="fontawesome/js/all.js"></script>
+
+    <script type="text/javacript" src="js/jquery.js"></script>
+    <script type="text/javacript" src="js/app.js"></script>
+
+    <title>tienda online</title>
+</head>
+<body >
+<div class="header">
+REPUESTOS Y AUTOPARTES CARS SITES
+</div> 
+
+<div class="menu">
+<a href="?p=principal"><img src="Imagen/homme.png">Inicio</a>
+<a href="?p=productos"><img src="Imagen/buscar.png">Productos</a>
+<a href="?p=ofertas"><img src="Imagen/oferta.ico">Ofertas</a>
+<a href="?p=carrito"><img src="Imagen/car.png">Carrito</a>
+
+<a class href="?p=admin"><img src="Imagen/user.png">Administrador</a>
+<a  href="?p=login"><img src="Imagen/userr.png">Usuario</a>
+
+<?php
+if(isset($_SESSION['id_cliente'])){
+?>
+<a class="pull-right subir " href="?p=salir">Salir</a>
+<a class="pull-right subir" href="#"><?=nombre_cliente($_SESSION['id_cliente'])?></a>
+<?php
+}
+?>
+</div> 
+
+
+
+
+<div class="cuerpo">  
+    <?php
+    if(file_exists("modulos/".$p.".php")){
+        include "modulos/".$p.".php";
+    }else{
+        echo "<i>No se ha ecncontrado el modulo <b>".$p. "</b><a href='./ '> Regresar</a></i>";
+    }
+    ?>
+
+</div>
+
+<div class="footer">
+   Tienda Online <br><br>
+
+</div>
+
+</body>
+
 </html>
---
+++ ofertas.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.79.0">
+    <title>Pricing example · Bootstrap v5.0</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/pricing/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="css/pricing.css" rel="stylesheet">
+  </head>
+  <body>
+    
+<header>
+</header>
+
+<main class="container">
+  <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+    <h1 class="display-4">Productos en oferta para tu vehículo</h1>
+    <p class="lead"></p>
+  </div>
+
+  <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+    <div class="col">
+      <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 fw-normal">Llanta Aro 16 205/55R16 PRIMEWELL</h4>
+      </div>
+      <div class="card-body">
+        <img class="card-title pricing-card-title" src="Imagen/llantass.webp">s/226 <small class="text-muted">/ Precio Antiguo</small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>Diseño: PS880</li>
+          <li>Medidas: 205/55R16</li>
+          <li>Indice de carga: 91</li>
+          <li>Indice de velocidad: V - 210 km/h</li>
+        </ul>
+        <button type="button" class="w-100 btn btn-lg btn-outline-primary">S/208.00</button>
+      </div>
+    </div>
+    </div>
+    <div class="col">
+      <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 fw-normal">Faros LED para automóviles H4 H1 H7</h4>
+      </div>
+      <div class="card-body">
+        <img class="card-title pricing-card-title" src="Imagen/foros.webp">s/59.00 <small class="text-muted">/ mo</small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>Tipo de chip LED: chips COB.</li>
+             <li> Vida útil: 35000 horas.</li>
+             <li> Potencia: 72W / Par, 36W / Bombilla.</li> 
+             <li> Temperatura de color:4300K luz cálida </li>
+
+            </ul>
+           <button type="button" class="w-100 btn btn-lg btn-primary">s/46.00</button>
+          </div>
+        </div>
+     </div>
+           
+  <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+    <div class="col">
+      <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 fw-normal">BATERIA HY H70600 (70AH/600A)</h4>
+      </div>
+      <div class="card-body">
+        <img class="card-title pricing-card-title" src="Imagen/bateriaa.webp">s/329.90 <small class="text-muted">/ Precio Antiguo</small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>Libre mantenimiento. </li>
+          <li>Cap. Nominal 166 Amperios/Hora.</li>
+          <li>Voltaje: 12 Voltios.</li>
+          <li>Capacidad de reserva 115 minutos</li>
+        </ul>
+        <button type="button" class="w-100 btn btn-lg btn-outline-primary">S/260.00</button>
+      </div>
+    </div>
+    </div>
+    <div class="col">
+      <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 fw-normal">Pegatinas puerta de coche anticolisión de Gaza </h4>
+      </div>
+      <div class="card-body">
+        <img class="card-title pricing-card-title" src="Imagen/pegatina.webp">s/65.46 <small class="text-muted">/ mo</small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>durable, resistente al agua, y no se borran.</li>
+             <li>Es conveniente para el vario tipo de coche.</li>
+             <li> Diseño portátil y simple, pequeño tamaño</li> 
+            
+
+            </ul>
+           <button type="button" class="w-100 btn btn-lg btn-primary">s/48.90</button>
+          </div>
+        </div>
+     </div>
+  <footer>
+  </footer>
+</main>
+ 
+  </body>
+</html>
